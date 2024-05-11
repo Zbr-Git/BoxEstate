03e666b5ee801bbed9e0efef5b6f415ba62bdeb0
Zubair <40324642+Zbr-Git@users.noreply.github.com>
2024-05-11 13:52:35 +0500

    styled Profile Page

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import './navbar.scss';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
   const [open, setOpen] = useState(false);
+
+  const user = true;
   return (
     <nav>
       <div className="left">
@@ -17,10 +19,26 @@ function Navbar() {
         <Link to="/agents">Agents</Link>
       </div>
       <div className="right">
-        <a href="/">Sign in</a>
-        <a href="/" className="register">
-          Sing up
-        </a>
+        {user ? (
+          <div className="user">
+            <img
+              src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+            />
+            <span>John Doe</span>
+            <Link to="/profile" className="profile">
+              <div className="notification">3</div>
+              <span>Profile</span>
+            </Link>
+          </div>
+        ) : (
+          <>
+            <a href="/">Sign in</a>
+            <a href="/" className="register">
+              Sing up
+            </a>
+          </>
+        )}
         <div className="menuIcon">
           <img src="/menu.png" onClick={() => setOpen((prev) => !prev)} />
         </div>
